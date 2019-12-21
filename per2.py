bb3b08bba65d35d47ebf857fdb687b4386b0545e
capitalismfree <muhammad.fausta@students.amikom.ac.id>
2019-12-21 13:35:52 +0700

    nothing

--- per2.py
+++ per2.py
@@ -1,10 +1,10 @@
 angka1 = int(input("masukkan angka pertama : "))
 angka2 = int(input("masukkan angka kedua : "))
-str(konfirmasi)
+konfirmasi = " "
 print("operator : \n1.penjumlahan\n2.pengurangan"
         "\n3.perkalian\n4.pembagian\n")
 pilihan = int(input("masukkan pilihan : "))
-while(konfirmasi != 'n' | konfirmasi != 'N'):
+while(konfirmasi != 'n' or konfirmasi != 'N'):
     if pilihan == 1:
         jawaban = angka1+angka2
         print("hasilnya adalah ",jawaban)
@@ -13,13 +13,17 @@ while(konfirmasi != 'n' | konfirmasi != 'N'):
         jawaban = angka1-angka2
         print("hasilnya adalah ",jawaban)
         konfirmasi = str(input("apakah anda ingin mengulang (y/n) : "))
+        if(konfirmasi == 'n' or konfirmasi == 'N'):
+            
     elif pilihan == 3:
         jawaban = angka1*angka2
         print("haslnya adalah ",jawaban)
         konfirmasi = str(input("apakah anda ingin mengulang (y/n) : "))
+        if(konfirmasi == 'n' or konfirmasi == 'N'):        
     elif pilihan == 4:
         jawaban = angka1/angka2
         print("hasilnya adalah ",jawaban)
         konfirmasi = str(input("apakah anda ingin mengulang (y/n) : "))
+        if(konfirmasi == 'n' or konfirmasi == 'N'):
     else:
         print("operator tidak ada")
